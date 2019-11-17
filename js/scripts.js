1b5afe61540bfa68b6e0dd620b1bbf84ea495afe
jennaturunen <jenna.turunen2@gmail.com>
2019-11-17 23:28:40 +0200

    scss toimii, sisaankirjaus/rekisterointi formi, eka versio karusellista

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+'use strict';
+
+// CHOOSE LOG IN OR REGISTRATION FORM
+let login_form = document.getElementById('login-form');
+let register_form = document.getElementById('register-form');
+let log_button = document.getElementById('switchFormButton');
+
+function move_register() {
+  login_form.style.left = '-400px';
+  register_form.style.left = '50px';
+  log_button.style.left = '110px';
+}
+
+function move_login() {
+  login_form.style.left = '50px';
+  register_form.style.left = '450px';
+  log_button.style.left = '0px';
+}
+
+// CHECK LOG IN VALUES
+function check_values() {
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('passwd').value;
+  const form_box = document.getElementById('form-box');
+  const hero = document.getElementById('hero');
+
+  if (username === 'kokeilu' && password === '123') {
+    console.log('toimii');
+    form_box.style.display = 'none';
+    hero.style.backgroundImage = "url('images/b_party.jpg')";
+  }
+  else {
+    console.log('oophs');
+    alert('The username and password you entered dont match');
+  }
+}
+
+// SLIDESHOW
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function selectSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Move between slides
+function showSlides(n) {
+  let slides = document.getElementsByClassName("carousel_slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+/*
+function check(form)
+{
+  if (form.uname.value ==="kokeilu" &&form.psw.value==="123"){
+    window.open('./media.html')
+  }
+  else{
+    alert(" the username and pasword you entered dont match " )
+  }
+}
+const modal= document.getElementById('signupModal');
+window.onclick= function(event) {
+  if (event.target===modal){
+    modal.style.display="none";
+  }
+};
+
+ */
