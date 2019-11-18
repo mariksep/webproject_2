8771a920b8125e9fd8fd19c7a76bcfd69e417f6b
maris <mari.seppanen4@metropolia.fi>
2019-11-18 16:30:33 +0200

    parantelua carouselin slide show

--- js/scripts.js
+++ js/scripts.js
@@ -50,7 +50,7 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
 
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 7000); // Change image every 5 seconds
 
 
 }
