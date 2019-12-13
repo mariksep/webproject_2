4f61a878206a8a77d8539c3651d7300ac4f1d437
maris <mari.seppanen4@metropolia.fi>
2019-12-13 10:31:24 +0200

    const url ja appp app.use vaihdettu

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const authRoute = require('./routes/authRoute');
 const likeRoute = require('./routes/likeRoute');
 
 app.use(cors());
-
+app.use(express.static('public'));
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
